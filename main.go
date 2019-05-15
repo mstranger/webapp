d0b513a04ffa20edadc1610a91f9e8305247cf00
mstranger <mr.mstranger@gmail.com>
2019-05-15 15:22:15 +0300

    Create the contact view template

--- main.go
+++ main.go
@@ -8,7 +8,10 @@ import (
 	"github.com/gorilla/mux"
 )
 
-var homeTemplate *template.Template
+var (
+	homeTemplate    *template.Template
+	contactTemplate *template.Template
+)
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -19,7 +22,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Contacts</h1><p>Contact page example.</p>")
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 }
 
 func faq(w http.ResponseWriter, r *http.Request) {
@@ -38,7 +43,12 @@ func main() {
 
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
 	if err != nil {
-		panic(nil)
+		panic(err)
+	}
+
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
+	if err != nil {
+		panic(err)
 	}
 
 	r := mux.NewRouter()
