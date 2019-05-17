0159c86179a629e611927c1507ae81cad5213a95
mstranger <mr.mstranger@gmail.com>
2019-05-17 18:25:52 +0300

    Add comments to Users. Delete obsolete signup.gohtml file

--- controllers/users.go
+++ controllers/users.go
@@ -7,6 +7,10 @@ import (
 	"../views"
 )
 
+// NewUsers is used to create a new Users controller.
+// This function will panic if the templates are not
+// parsed correctly, and should only be used during
+// initial setup.
 func NewUsers() *Users {
 	return &Users{
 		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
