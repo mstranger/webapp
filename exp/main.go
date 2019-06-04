d0b61c46e6fbff6a290661c1ae41e7fb792ecbf0
mstranger <mr.mstranger@gmail.com>
2019-06-04 21:54:36 +0300

    Handle errors when querying

--- exp/main.go
+++ exp/main.go
@@ -36,17 +36,54 @@ func main() {
 	db.LogMode(true)
 	db.AutoMigrate(&User{})
 
-	var u User
+	// var u User
 	// newdb := db.Where("id = ? AND color = ?", 4, "red")
 	// newdb.First(&u)
-	db.Where("color = ?", "red").
-		Where("id > ?", 4).
-		First(&u)
+	// db.Where("color = ?", "red").
+	// 	Where("id > ?", 4).
+	// 	First(&u)
 	// db.First(&u, "color = ?", "red")
-	fmt.Println(u)
+	// fmt.Println(u)
 
-	var users []User
-	db.Find(&users)
-	fmt.Println(len(users))
-	fmt.Println(users)
+	var u User
+	// newDB := db.Where("email = ?", "blabla@bla.com")
+	// newDB = newDB.Or("color = ?", "red")
+	// newDB = newDB.First(&u)
+
+	// db = db.Where("email = ? ", "blabla@bla.com").First(&u)
+	if err := db.Where("email = ?", "bla@bla.com").First(&u).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			fmt.Println("No user found!")
+		case gorm.ErrInvalidSQL:
+			fmt.Println("Invalid query!")
+		default:
+			panic(err)
+		}
+	}
+	fmt.Println(u)
+	// errors := db.GetErrors()
+	// if db.RecordNotFound() {
+	// 	fmt.Println("No user found!")
+	// } else if db.Error != nil {
+	// 	panic(db.Error)
+	// } else {
+	// 	fmt.Println(u)
+	// }
+
+	// if len(errors) > 0 {
+	// 	fmt.Println(errors)
+	// 	os.Exit(1)
+	// }
+
+	// if db.Error != nil {
+	// panic(db.Error)
+	// }
+
+	// fmt.Println(u)
+
+	// var users []User
+	// db.Find(&users)
+	// fmt.Println(len(users))
+	// fmt.Println(users)
 }
